32a7cd1359d866862943a37a14ffef80bf989263
Shubhankar <das.shubhankar97@gmail.com>
2020-09-27 23:24:51 +0530

    added env vars

---
+++ src/firebase-config.js
@@ -0,0 +1,12 @@
+const config = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: "chat-app-878e3.firebaseapp.com",
+  databaseURL: "https://chat-app-878e3.firebaseio.com",
+  projectId: "chat-app-878e3",
+  storageBucket: "chat-app-878e3.appspot.com",
+  messagingSenderId: "75550798120",
+  appId: "1:75550798120:web:3b7aa0a14eb68fb766201d",
+  measurementId: "G-X3LSYNRQJ0",
+};
+
+module.exports.config = config;
