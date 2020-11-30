262812fb7b688f7b20f5adb6a1a634d4f250b59e
Shubhankar <das.shubhankar97@gmail.com>
2020-11-30 22:55:39 +0530

    added build files

---
+++ build/precache-manifest.ea3b32a14630096a44df0dabd9a6dc44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2ffcc331f313893b47353add09cc03b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "718191fbee998f124303",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "19f767bb369764a2d953",
+    "url": "/static/js/2.620ab5aa.chunk.js"
+  },
+  {
+    "revision": "cfcdebbd5c361eb80dad4bc286f1bc4f",
+    "url": "/static/js/2.620ab5aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "718191fbee998f124303",
+    "url": "/static/js/main.8cb9473b.chunk.js"
+  },
+  {
+    "revision": "3cf8ae71a09948a08a2e",
+    "url": "/static/js/runtime-main.4857cacc.js"
+  }
+]);
